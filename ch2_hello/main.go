116cdae9d74391807c94de1d50ec71bb6eff415d
wegamekinglc <scrappedprince.li@gmail.com>
2021-06-06 15:03:22 +0800

    FEATURE: added ch4 example

--- ch2_hello/main.go
+++ ch2_hello/main.go
@@ -8,9 +8,11 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, 世界")
+	log.Println("This is my hello world example")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
